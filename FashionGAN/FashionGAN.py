8915bee8e249202329b5e008bacf819df789cfde
Maxwell Carlson <bart4282819@gmail.com>
2021-05-30 20:09:52 -0700

    Starting custom training loop

--- FashionGAN/FashionGAN.py
+++ FashionGAN/FashionGAN.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import keras as K
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras import models, layers
 from sklearn.decomposition import PCA
@@ -44,43 +45,81 @@ testX, testY = load_mnist('data', kind='t10k')
 trainX = np.divide(trainX, 255)
 testX = np.divide(testX, 255)
 
-
-genin = K.Input(shape=(100))
+generatorInputSize = 100
+genin = K.Input(shape=(generatorInputSize))
+genlayers = []
 x = layers.Dense(256)(genin)
+genlayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(512)(x)
+genlayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(648)(x)
+genlayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(784)(x)
+genlayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 genout = layers.Reshape((28,28,1))(x)
 
 ganGen = K.Model(genin, genout)
 
-descrimin = K.Input(shape=(28,28,1))
-x = layers.Reshape((784,))(descrimin)
+deslayers = []
+desin = K.Input(shape=(28,28,1))
+x = layers.Reshape((784,))(desin)
 x = layers.Dense(784)(x)
+deslayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(512)(x)
+deslayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(256)(x)
+deslayers.append(x)
 x = layers.LeakyReLU()(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dense(1)(x)
+deslayers.append(x)
 x = K.activations.sigmoid(x)
-descrimout = layers.BatchNormalization()(x)
+desout = layers.BatchNormalization()(x)
 
-ganDescrim = K.Model(descrimin, descrimout)
-
+ganDescrim = K.Model(desin, desout)
 ganGen.summary()
 ganDescrim.summary()
 
-GAN = K.layers.Concatenate()([ganGen, ganDescrim])
+GAN = K.models.Sequential(layers=[ganGen, ganDescrim])
 
+GAN.compile(optimizer=K.optimizers.Adam(learning_rate=0.001), 
+        loss=K.losses.binary_crossentropy,
+        metrics=[])
+
+
+epochs = 2
+batchSize = 32
+optim = K.optimizers.Adam()
+lossfn = K.losses.BinaryCrossentropy()
+
+#GAN.fit(d1, d2, batchSize, epochs)
+
+trainData = tf.data.Dataset.from_tensor_slices((trainX, np.ones((trainX.shape[0], 1))))
+
+for e in range(epochs):
+
+    for b, (X, Y) in enumerate(trainData):
+        #mvns = np.random.multivariate_normal(mean=np.zeros(100), cov=np.diag(np.ones(100)), 
+        #                                     size=(batchSize, 1))
+        mvns = np.random.rand(batchSize, generatorInputSize)
+
+        # Generate images from the generator
+        genImages = ganGen.predict(mvns)        
+
+
+
+
+
+        a=5
